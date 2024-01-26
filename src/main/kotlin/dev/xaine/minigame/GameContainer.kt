b1ca4162cffacf4d17a0b39692bdfe5f037f4382
xai <xai@xaine.dev>
2024-01-26 13:28:40 +0000

    benchmark test, getting game from id string

--- src/main/kotlin/dev/xaine/minigame/GameContainer.kt
+++ src/main/kotlin/dev/xaine/minigame/GameContainer.kt
@@ -9,16 +9,22 @@ import kotlin.reflect.full.memberProperties
 class GameContainer {
 
     val GAME_MAP = HashMap<KClass<out Game>, Game>()
+    val ID_MAP = HashMap<String, KClass<out Game>>()
     init {
         addGame(GameUltraSequencer::class)
     }
 
-    private fun addGame(game: KClass<out Game>) {
-        val gameInstance = game.createInstance()
-        GAME_MAP[gameInstance::class] = gameInstance
+    private fun addGame(gameClass: KClass<out Game>) {
+        val gameInstance = gameClass.createInstance()
+        GAME_MAP[gameClass] = gameInstance
+        ID_MAP[gameInstance.getGameID()] = gameClass
     }
 
     inline fun <reified T: Game> getGame(): T? {
         return GAME_MAP[T::class] as? T
+    }
+
+    fun getGameFromID(gameID: String): Game? {
+        return GAME_MAP[ID_MAP[gameID]]
     }
 }
--- src/test/kotlin/game/GameInstantiateTest.kt
+++ src/test/kotlin/game/GameInstantiateTest.kt
@@ -2,20 +2,60 @@ package game
 
 import dev.xaine.minigame.GameContainer
 import dev.xaine.minigame.impl.GameUltraSequencer
+import java.time.LocalDate
+import java.time.LocalDateTime
 import kotlin.test.Test
 
 class GameInstantiateTest {
+
     @Test
-    fun test() {
-        log("running test")
+    fun testFromClass() {
         val gameContainer = GameContainer()
-        log("created game container")
         val ultraSequencer = gameContainer.getGame<GameUltraSequencer>()
-        log("fetched ultra sequencer game")
         assert(ultraSequencer != null)
-        log("asserted game is not null")
         val gameID = ultraSequencer?.getGameID()
-        log("found game ${ultraSequencer!!::class} with id $gameID")
+        assert(!gameID.isNullOrEmpty())
+    }
+
+    @Test
+    fun testFromID() {
+        val gameContainer = GameContainer()
+        val ultraSequencer = gameContainer.getGameFromID("ultrasequencer")
+        assert(ultraSequencer != null)
+        val gameID = ultraSequencer?.getGameID()
+        assert(!gameID.isNullOrEmpty())
+    }
+
+    @Test
+    fun benchmarkTest() {
+        val gameContainer = GameContainer()
+
+        val testLength = 1000
+        val tests = 100
+        val testData = Array<Long>(tests){0}
+        for (test in 0..<tests) {
+            for (index in 0..<testLength) {
+                val initTime = System.nanoTime();
+
+                run {
+                    val ultraSequencer = gameContainer.getGame<GameUltraSequencer>()
+                    assert(ultraSequencer != null)
+                }
+
+                val endTime = System.nanoTime()
+                testData[test] = endTime-initTime
+            }
+        }
+        println("Average Time: ${average(testData)}")
+    }
+
+    private fun average(input: Array<Long>): Long {
+        var total = 0L
+        for (value in input) {
+            total += value
+        }
+        total /= input.size
+        return total
     }
 
     private fun log(message: String) {
